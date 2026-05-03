527a5f936fb09e5a4559d44a7c0dc27ba25b88b3
i2522269-ctrl <i2522269@continental.edu.pe>
2026-05-02 19:08:36 -0500

    feat: implementar modelo Libro con atributos privados (POO)

---
+++ models/libro.py
@@ -0,0 +1,49 @@
+class Libro:
+    def __init__(self, id, titulo, autor, precio_costo, precio_venta, stock):
+        self.__id = id
+        self.__titulo = titulo
+        self.__autor = autor
+        self.__precio_costo = precio_costo
+        self.__precio_venta = precio_venta
+        self.__stock = stock
+
+    # Getters
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @property
+    def autor(self):
+        return self.__autor
+
+    @property
+    def precio_costo(self):
+        return self.__precio_costo
+
+    @property
+    def precio_venta(self):
+        return self.__precio_venta
+
+    @property
+    def stock(self):
+        return self.__stock
+
+    # Setters (ejemplo para stock)
+    @stock.setter
+    def stock(self, nuevo_stock):
+        if nuevo_stock >= 0:
+            self.__stock = nuevo_stock
+
+    # Lógica de costos básica
+    def calcular_margen(self):
+        return self.__precio_venta - self.__precio_costo
+
+    def valor_inventario(self):
+        return self.__precio_costo * self.__stock
+
+    def __str__(self):
+        return f"Libro: {self.__titulo} - Stock: {self.__stock}"
